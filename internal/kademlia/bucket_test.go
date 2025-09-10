607604451ff3aa10bd7a1ebfa2a6a5f732fd0842
Simon <simon.ruskola@gmail.com>
2025-09-10 10:31:04 +0200

    cleanup

--- internal/kademlia/bucket_test.go
+++ internal/kademlia/bucket_test.go
@@ -5,18 +5,17 @@ import (
 	"testing"
 )
 
-func TestBucket(t *testing.T) {
-
+func TestBucketInitialization(t *testing.T) {
 	bucket := newBucket()
 	if bucket == nil {
 		t.Error("Expected bucket to be initialized, got nil")
 	}
+}
 
+func TestAddContactToBucket(t *testing.T) {
+	bucket := newBucket()
 	contact := NewContact(NewKademliaID("0000000000000000000000000000000000000001"), "localhost:8001")
-
 	bucket.AddContact(contact)
-
-	//check if contact is added in front of the bucket
 	firstElement := bucket.list.Front()
 	if firstElement == nil {
 		t.Errorf("Expected contact to be added, but bucket is empty")
@@ -26,33 +25,32 @@ func TestBucket(t *testing.T) {
 			t.Errorf("Expected contact to be at the front of the bucket, got %s", firstContact.ID.String())
 		}
 	}
+}
 
+func TestBucketLength(t *testing.T) {
+	bucket := newBucket()
 	amountOfContacts := 5
-
 	for i := 0; i < amountOfContacts; i++ {
 		stringI := fmt.Sprintf("%02d", i)
 		contact := NewContact(NewKademliaID("00000000000000000000000000000000000000"+stringI), "localhost:800"+stringI)
 		bucket.AddContact(contact)
 	}
-
-	//check if the length of the bucket is correct
 	if bucket.Len() != amountOfContacts {
 		t.Errorf("Expected bucket length to be %d, got %d", amountOfContacts, bucket.Len())
 	}
+}
 
-	//check if the order is correct
-
+func TestBucketContactOrder(t *testing.T) {
+	bucket := newBucket()
+	amountOfContacts := 5
 	expectedOrder := []string{}
-	
-
 	for i := 0; i < amountOfContacts; i++ {
 		stringI := fmt.Sprintf("%02d", i)
 		contactString := "00000000000000000000000000000000000000" + stringI
+		contact := NewContact(NewKademliaID(contactString), "localhost:800"+stringI)
+		bucket.AddContact(contact)
 		expectedOrder = append([]string{contactString}, expectedOrder...)
-
 	}
-	
-
 	i := 0
 	for e := bucket.list.Front(); e != nil; e = e.Next() {
 		contact := e.Value.(Contact)
@@ -61,6 +59,5 @@ func TestBucket(t *testing.T) {
 		}
 		i++
 	}
-
 }
 
--- internal/kademlia/routingtable_test.go
+++ internal/kademlia/routingtable_test.go
@@ -6,52 +6,40 @@ import (
 )
 
 
-func TestRoutingTable(t *testing.T) {
-
-
-	amountOfContacts := 16
-
+func TestFindClosestContact(t *testing.T) { // should probalbly be updated to include more contacts
 	rt := NewRoutingTable(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
+	contact := NewContact(NewKademliaID("0000000100000000000000000000000000000000"), "localhost:8001")
+	rt.AddContact(contact)
+	closestContact := rt.FindClosestContacts(NewKademliaID("0000000100000000000000000000000000000000"), 1)
 
+	if closestContact[0].ID.String() != "0000000100000000000000000000000000000000" {
+		t.Fatalf("Expected closest contact to be 0000000100000000000000000000000000000000, got %s", closestContact[0].ID.String())
+	}
+}
+
+func TestRoutingTableContactCount(t *testing.T) {
+	amountOfContacts := 16
+	rt := NewRoutingTable(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
 	for i := 0; i < amountOfContacts; i++ {
 		stringI := fmt.Sprintf("%02d", i)
 		contact := NewContact(NewKademliaID("0000000"+stringI+"00000000000000000000000000000000"), "localhost:800"+stringI)
 		rt.AddContact(contact)
-		rt.AddContact(contact) // adding duplicate contact
 	}
-
-	
-	//Check closest contact is correct
-
-	closestContact := rt.FindClosestContacts(NewKademliaID("0000000100000000000000000000000000000000"), 1)
-	
-	if closestContact[0].ID.String() != "0000000100000000000000000000000000000000" {
-		t.Fatalf("Expected closest contact to be 0000000100000000000000000000000000000000, got %s", closestContact[0].ID.String())
-	}
-	
-
-
-	
 	contacts := rt.FindClosestContacts(NewKademliaID("2111111400000000000000000000000000000000"), amountOfContacts)
-
-
-	for i := range contacts {
-	
-		fmt.Println(contacts[i].String())
-	} 
-
-	fmt.Println("Total contacts found:", len(contacts))
-
-
-	//Check correct amount of contacts are added
-
 	if len(contacts) != amountOfContacts {
 		t.Fatalf("Expected %d contacts, got %d", amountOfContacts, len(contacts))
 	}
+}
 
-
-	//check make sure the correct contacts are added to the routing table
-
+func TestRoutingTableCorrectContactsAdded(t *testing.T) {
+	amountOfContacts := 16
+	rt := NewRoutingTable(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
+	for i := 0; i < amountOfContacts; i++ {
+		stringI := fmt.Sprintf("%02d", i)
+		contact := NewContact(NewKademliaID("0000000"+stringI+"00000000000000000000000000000000"), "localhost:800"+stringI)
+		rt.AddContact(contact)
+	}
+	contacts := rt.FindClosestContacts(NewKademliaID("2111111400000000000000000000000000000000"), amountOfContacts)
 	for i := 0; i < amountOfContacts; i++ {
 		stringI := fmt.Sprintf("%02d", i)
 		expectedContact := NewContact(NewKademliaID("0000000"+stringI+"00000000000000000000000000000000"), "localhost:800"+stringI)
@@ -66,16 +54,23 @@ func TestRoutingTable(t *testing.T) {
 			t.Fatalf("Expected contact %s not found", expectedContact.String())
 		}
 	}
+}
 
-	//check that the there are no duplicates in the routingtable
-
+func TestRoutingTableNoDuplicateContacts(t *testing.T) {
+	amountOfContacts := 16
+	rt := NewRoutingTable(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
+	for i := 0; i < amountOfContacts; i++ {
+		stringI := fmt.Sprintf("%02d", i)
+		contact := NewContact(NewKademliaID("0000000"+stringI+"00000000000000000000000000000000"), "localhost:800"+stringI)
+		rt.AddContact(contact)
+		rt.AddContact(contact)
+	}
+	contacts := rt.FindClosestContacts(NewKademliaID("2111111400000000000000000000000000000000"), amountOfContacts)
 	uniqueContacts := make(map[string]struct{})
 	for _, contact := range contacts {
 		uniqueContacts[contact.ID.String()] = struct{}{}
 	}
 	if len(uniqueContacts) != len(contacts) {
-		t.Fatal("Expected no duplicate contacts") 
+		t.Fatal("Expected no duplicate contacts")
 	}
-
-
 }
